84947cddab4b3404fb71f7e98327900b1b12c0c9
Louis Ellis <louisellis@resnet161-140.medford.tufts.edu>
2012-03-26 00:15:52 -0400

    Initial Commit

---
+++ Instrument/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Instrument
+//
+//  Created by Louis Ellis on 3/26/12.
+//  Copyright (c) 2012 Tufts University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
